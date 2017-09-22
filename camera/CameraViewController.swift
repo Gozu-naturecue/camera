fca2b85d32f33ccb6224ea94aac44d8b515e6682
Yuhsuke Gozu <Yuhsuke_Gozu@nature-cue.com>
2017-09-22 23:44:08 +0900

    [edit]カメラを動画モードに変更

--- camera/CameraViewController.swift
+++ camera/CameraViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, UIGestureRecognizerDelegate {
     
     private var shutterLabel:UILabel!
     private var shutterButton:UIButton!
@@ -17,7 +17,7 @@ class CameraViewController: UIViewController {
     private var cameraView:UIView!
     
     private var blackView:UIView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,7 +29,7 @@ class CameraViewController: UIViewController {
         cameraView.frame = CGRect(x: 0, y: 50, width: self.view.frame.width, height: self.view.frame.width/3 * 4)
         cameraView.backgroundColor = UIColor.white
         self.view.addSubview(cameraView)
-
+        
         // シャッターの外郭
         shutterLabel = UILabel()
         
@@ -70,52 +70,6 @@ class CameraViewController: UIViewController {
         blackView.isHidden = true
         self.view.addSubview(blackView)
     }
-
-    var captureSesssion: AVCaptureSession!
-    var stillImageOutput: AVCapturePhotoOutput?
-    var previewLayer: AVCaptureVideoPreviewLayer?
-    override func viewWillAppear(_ animated: Bool) {
-        captureSesssion = AVCaptureSession()
-        stillImageOutput = AVCapturePhotoOutput()
-        
-        captureSesssion.sessionPreset = AVCaptureSession.Preset.photo // 解像度の設定
-        
-        let device =  AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .back)
-        
-        do {
-            let input = try AVCaptureDeviceInput(device: device!)
-            
-            // 入力
-            if (captureSesssion.canAddInput(input)) {
-                captureSesssion.addInput(input)
-                
-                // 出力
-                if (captureSesssion.canAddOutput(stillImageOutput!)) {
-                    captureSesssion.addOutput(stillImageOutput!)
-                    captureSesssion.startRunning() // カメラ起動
-                    
-                    previewLayer = AVCaptureVideoPreviewLayer(session: captureSesssion)
-                    previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect // アスペクトフィット
-                    previewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait // カメラの向き
-                    
-                    self.cameraView.layer.addSublayer(previewLayer!)
-                    
-                    // ビューのサイズの調整
-                    previewLayer?.position = CGPoint(x: self.cameraView.frame.width / 2, y: self.cameraView.frame.height / 2)
-                    previewLayer?.bounds = self.cameraView.frame
-                }
-            }
-        }
-        catch {
-            print(error)
-        }
-
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @objc internal func onDownShutterButton(sender: UIButton) {
         UIView.animate(withDuration: 0.06,
@@ -125,7 +79,7 @@ class CameraViewController: UIViewController {
 
             // 縮小用アフィン行列を作成する.
             self.shutterButton.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
-            
+                
         })
         { (Bool) -> Void in
             
@@ -143,6 +97,88 @@ class CameraViewController: UIViewController {
         }, completion: { _ in
             self.blackView.isHidden = true
         })
-
+    }
+    
+    
+    
+    var input:AVCaptureDeviceInput!
+    var output:AVCaptureVideoDataOutput!
+    var session:AVCaptureSession!
+    var camera:AVCaptureDevice!
+    var imageView:UIImageView!
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    override func viewWillAppear(_ animated: Bool) {
+        // カメラの設定
+        setupCamera()
+    }
+    
+    // メモリ解放
+    override func viewDidDisappear(_ animated: Bool) {
+        // camera stop メモリ解放
+        session.stopRunning()
+        
+        for output in session.outputs {
+            session.removeOutput(output)
+        }
+        
+        for input in session.inputs {
+            session.removeInput(input)
+        }
+        session = nil
+        camera = nil
+    }
+    
+    func setupCamera(){
+        // AVCaptureSession: キャプチャに関する入力と出力の管理
+        session = AVCaptureSession()
+        
+        // sessionPreset: キャプチャ・クオリティの設定
+        session.sessionPreset = AVCaptureSession.Preset.photo
+        
+        // 背面・前面カメラの選択 iOS10での変更
+        camera = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .back)
+        
+        // カメラからの入力データ
+        do {
+            input = try AVCaptureDeviceInput(device: camera) as AVCaptureDeviceInput
+        } catch let error as NSError {
+            print(error)
+        }
+        
+        // 入力をセッションに追加
+        if(session.canAddInput(input)) {
+            session.addInput(input)
+        }
+        
+        // AVCaptureVideoDataOutput :動画フレームデータを出力に設定
+        output = AVCaptureVideoDataOutput()
+        // 出力をセッションに追加
+        if(session.canAddOutput(output)) {
+            session.addOutput(output)
+            session.stopRunning()
+            
+            previewLayer = AVCaptureVideoPreviewLayer(session: session)
+            previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect // アスペクトフィット
+            previewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait // カメラの向き
+            
+            self.cameraView.layer.addSublayer(previewLayer!)
+            
+            // ビューのサイズの調整
+            previewLayer?.position = CGPoint(x: self.cameraView.frame.width / 2, y: self.cameraView.frame.height / 2)
+            previewLayer?.bounds = self.cameraView.frame
+        }
+        
+        // ピクセルフォーマットを 32bit BGR + A とする
+        output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as AnyHashable as! String : Int(kCVPixelFormatType_32BGRA)]
+        
+        // フレームをキャプチャするためのサブスレッド用のシリアルキューを用意
+        output.setSampleBufferDelegate(self, queue: DispatchQueue.main)
+        
+        output.alwaysDiscardsLateVideoFrames = true
+        
+        // ビデオ出力に接続
+        // let connection  = output.connection(withMediaType: AVMediaTypeVideo)
+        
+        session.startRunning()
     }
 }
