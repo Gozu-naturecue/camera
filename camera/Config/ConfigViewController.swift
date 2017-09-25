33946b8883d2eff59c4f43a65a524a1615bd6516
Yuhsuke Gozu <Yuhsuke_Gozu@nature-cue.com>
2017-09-25 20:23:59 +0900

    コンフィグ画面作成完了

--- camera/Config/ConfigViewController.swift
+++ camera/Config/ConfigViewController.swift
@@ -26,6 +26,12 @@ class ConfigViewController: UIViewController, UITableViewDelegate, UITableViewDa
         button.setImage(UIImage(named: "camera"), for: .normal)
         return button
     }()
+    let borderUnderHeader: CALayer = {
+        let layer = CALayer()
+        layer.frame = CGRect(x: 0, y: 50, width: UIScreen.main.bounds.size.width, height: 1.0)
+        layer.backgroundColor = #colorLiteral(red: 0.1607843137, green: 0.1607843137, blue: 0.1607843137, alpha: 1)
+        return layer
+    }()
     let titleLabel: UILabel = {
         let label = UILabel()
         label.textColor = UIColor.white
@@ -37,6 +43,7 @@ class ConfigViewController: UIViewController, UITableViewDelegate, UITableViewDa
         let table = UITableView.init(frame: CGRect(x: 50, y: 0, width: 0, height: 0), style: UITableViewStyle(rawValue: 1)!)
         table.backgroundColor = .black
         table.separatorInset = UIEdgeInsets.zero
+        table.separatorColor = #colorLiteral(red: 0.1607843137, green: 0.1607843137, blue: 0.1607843137, alpha: 1)
         return table
     }()
     
@@ -45,8 +52,9 @@ class ConfigViewController: UIViewController, UITableViewDelegate, UITableViewDa
         self.view.backgroundColor = .black
         
         view.addSubview(headerView)
-        headerView.addSubview(titleLabel)
-        headerView.addSubview(homeButton)
+//            headerView.layer.addSublayer(borderUnderHeader)
+            headerView.addSubview(titleLabel)
+            headerView.addSubview(homeButton)
         view.addSubview(tableView)
         
         headerView.snp.makeConstraints({ (make) in
@@ -64,7 +72,7 @@ class ConfigViewController: UIViewController, UITableViewDelegate, UITableViewDa
         })
         tableView.snp.makeConstraints({ (make) in
             make.width.equalToSuperview()
-            make.top.equalTo(headerView.snp.bottom).offset(20)
+            make.top.equalTo(headerView.snp.bottom)
             make.bottom.left.equalTo(0)
         })
         
@@ -125,7 +133,7 @@ class ConfigViewController: UIViewController, UITableViewDelegate, UITableViewDa
     // リストのアイテム
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.contentView.backgroundColor = #colorLiteral(red: 0.05098039216, green: 0.05098039216, blue: 0.05098039216, alpha: 1)
+        cell.contentView.backgroundColor = #colorLiteral(red: 0.09019607843, green: 0.09019607843, blue: 0.09019607843, alpha: 1)
         cell.selectionStyle = UITableViewCellSelectionStyle.none
         cell.textLabel?.textColor = UIColor.white
         cell.accessoryType = .none
