9f988bf589e8323df2e3c50fc73342a9afdd0e60
Yuhsuke Gozu <Yuhsuke_Gozu@nature-cue.com>
2017-09-29 13:08:20 +0900

    選択した画像を切り取り用画面に表示

--- camera/Config/CropImageViewController.swift
+++ camera/Config/CropImageViewController.swift
@@ -12,20 +12,10 @@ import UIKit
 class CropImageViewController: SuperViewController {
     var image: UIImage!
 
-    let imageView: UIImageView = {
+    var imageView: UIImageView = {
        let imageView = UIImageView()
         imageView.backgroundColor = .red
         return imageView
-    }()
-    
-    let backButton: UIButton = {
-        let button = UIButton()
-        button.layer.masksToBounds = true
-        button.layer.cornerRadius = CGFloat(35)
-        button.setTitle("BACK", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.3042594178)
-        return button
     }()
     
     let doneButton: UIButton = {
@@ -40,8 +30,7 @@ class CropImageViewController: SuperViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(imageView)
-        view.addSubview(backButton)
+        setImage()
         view.addSubview(doneButton)
         
         imageView.snp.makeConstraints({ (make) in
@@ -49,34 +38,32 @@ class CropImageViewController: SuperViewController {
             make.top.left.equalTo(0)
         })
         
-        backButton.snp.makeConstraints({ (make) in
-            make.width.height.equalTo(70)
-            make.bottom.equalTo(0).offset(-25)
-            make.left.equalTo(0).offset(25)
-        })
-        
         doneButton.snp.makeConstraints({ (make) in
             make.width.height.equalTo(70)
-            make.bottom.right.equalTo(0).offset(-25)
+            make.bottom.equalTo(0).offset(-25)
+            make.centerX.equalToSuperview()
         })
-        
-        backButton.addTarget(self, action: #selector(CropImageViewController.onDownBackButton(sender:)), for: .touchDown)
-        backButton.addTarget(self, action: #selector(CropImageViewController.onUpBackButton(sender:)), for: [.touchUpInside,.touchUpOutside])
+
         doneButton.addTarget(self, action: #selector(CropImageViewController.onDownDoneButton(sender:)), for: .touchDown)
         doneButton.addTarget(self, action: #selector(CropImageViewController.onUpDoneButton(sender:)), for: [.touchUpInside,.touchUpOutside])
-
+        
+        self.edgesForExtendedLayout = []
     }
 
+    /**
+     選択された画像をUIImageViewにセットする.
+     */
+    func setImage(){
+        self.title = "Selected Image"
+        
+        imageView = UIImageView(frame: self.view.bounds)
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
+        imageView.image = image
+        self.view.addSubview(imageView)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    @objc internal func onDownBackButton(sender: UIButton) {
-        self.backButton.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5)
-    }
-    
-    @objc internal func onUpBackButton(sender: UIButton) {
-        self.backButton.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.3042594178)
     }
     
     @objc internal func onDownDoneButton(sender: UIButton) {
